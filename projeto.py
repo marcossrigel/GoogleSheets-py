0f64720492d02837b2aed45a506d525218d5702c
Marcos Rigel <marcosrigel1999@gmail.com>
2025-04-02 16:45:31 -0300

    melhor versao 1.3

--- projeto.py
+++ projeto.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import pyautogui
 import webbrowser
+import time
 import re
 
 url = 'https://docs.google.com/spreadsheets/d/1p5GkS4ngPcai_U8p2rOKnj3_kiefYYv383L6oevaNmM/export?format=csv'
 df = pd.read_csv(url, usecols=['ID', 'Nome', 'Telefone'])
 
 resultado = df.loc[df['ID'] == 4].squeeze()
+
 nome = resultado['Nome']
 numero = re.sub(r'\D', '', resultado['Telefone'])
 
 webbrowser.open(f"https://web.whatsapp.com/send?phone={numero}&text=Olá {nome}, esta é uma mensagem automática :)")
+
+time.sleep(5)
 pyautogui.press('enter')

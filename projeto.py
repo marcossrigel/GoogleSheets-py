b6642f5f963bf2ce524ba326beb77bf559b05f4c
Marcos Rigel <marcosrigel1999@gmail.com>
2025-04-02 15:40:37 -0300

    Melhor versão 1.0

--- projeto.py
+++ projeto.py
@@ -6,16 +6,16 @@ import re
 
 df = pd.read_csv('https://docs.google.com/spreadsheets/d/1p5GkS4ngPcai_U8p2rOKnj3_kiefYYv383L6oevaNmM/export?format=csv')
 
-resultado = df[df['ID'] == 3]
+resultado = df[df['ID'] == 2]
 nome = resultado['Nome'].values[0]
 telefone = resultado['Telefone'].values[0]
 
 numero = re.sub(r'\D', '', telefone)
 
 hora_atual = datetime.datetime.now()
-
 numero_da_hora = hora_atual.hour
 numero_do_minuto = hora_atual.minute
+
 minuto_mais_1 = numero_do_minuto + 1
 
 if minuto_mais_1 == 60:

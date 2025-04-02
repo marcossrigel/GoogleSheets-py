8ca872b8c99fdc43bc66fd7f21a67b1fc36c24ac
marcossrigel <marcosrigel1999@gmail.com>
2025-04-02 10:22:22 -0300

    novas atualizacoes

--- projeto.py
+++ projeto.py
@@ -4,9 +4,12 @@ url = 'https://docs.google.com/spreadsheets/d/1p5GkS4ngPcai_U8p2rOKnj3_kiefYYv38
 
 df = pd.read_csv(url)
 
-id_procurado = 1
-
+id_procurado = 5
 resultado = df[df['ID'] == id_procurado]
+
+telefone = resultado['Telefone'].values[0]
+nome = resultado['Nome'].values[0]
+print(f"Telefone {nome}: {telefone}")
 
 if not resultado.empty:
     print(resultado.to_string(index=False))

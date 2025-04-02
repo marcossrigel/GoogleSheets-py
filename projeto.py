46401f6565cbc7643bb61989aa43cb127d8295bd
Marcos Rigel <marcosrigel1999@gmail.com>
2025-04-02 17:00:13 -0300

    melhor versao 1.3

--- projeto.py
+++ projeto.py
@@ -7,12 +7,12 @@ import re
 url = 'https://docs.google.com/spreadsheets/d/1p5GkS4ngPcai_U8p2rOKnj3_kiefYYv383L6oevaNmM/export?format=csv'
 df = pd.read_csv(url, usecols=['ID', 'Nome', 'Telefone'])
 
-resultado = df.loc[df['ID'] == 4].squeeze()
+resultado = df.loc[df['ID'] == 3].squeeze()
 
 nome = resultado['Nome']
 numero = re.sub(r'\D', '', resultado['Telefone'])
 
-webbrowser.open(f"https://web.whatsapp.com/send?phone={numero}&text=Olá {nome}, esta é uma mensagem automática :)")
+webbrowser.open(f'https://web.whatsapp.com/send?phone={numero}&text=Olá {nome}, esta é uma mensagem automática :)')
 
 time.sleep(5)
 pyautogui.press('enter')

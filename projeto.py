b7582e1524f45f9acc5d5111eef4830f24e68dc2
Marcos Rigel <marcosrigel1999@gmail.com>
2025-04-02 12:38:53 -0300

    Novas atualizacoes

--- projeto.py
+++ projeto.py
@@ -1,17 +1,10 @@
 import pandas as pd
 
-url = 'https://docs.google.com/spreadsheets/d/1p5GkS4ngPcai_U8p2rOKnj3_kiefYYv383L6oevaNmM/export?format=csv'
+df = pd.read_csv('https://docs.google.com/spreadsheets/d/1p5GkS4ngPcai_U8p2rOKnj3_kiefYYv383L6oevaNmM/export?format=csv')
 
-df = pd.read_csv(url)
-
-id_procurado = 5
-resultado = df[df['ID'] == id_procurado]
+resultado = df[df['ID'] == 1]
 
 telefone = resultado['Telefone'].values[0]
 nome = resultado['Nome'].values[0]
 print(f"Telefone {nome}: {telefone}")
 
-if not resultado.empty:
-    print(resultado.to_string(index=False))
-else:
-    print("ID não encontrado.")

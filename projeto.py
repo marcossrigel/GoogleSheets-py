1dedc4463a81b0be98cc5e901100088eb5d4c852
marcossrigel <marcosrigel1999@gmail.com>
2025-04-02 08:59:39 -0300

    Integracao google sheets api

--- projeto.py
+++ projeto.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
-sheet_id = "1AbCdeFgHiJKLmnopQRStuvWXyZ1234567890"  # Substitua pelo ID real
-sheet_name = "Página1"  # Substitua pelo nome real da aba
-
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+url = 'https://docs.google.com/spreadsheets/d/1p5GkS4ngPcai_U8p2rOKnj3_kiefYYv383L6oevaNmM/export?format=csv'
 
 df = pd.read_csv(url)
 
-print(df.head())
+df.head()

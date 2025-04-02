f3c57322a41b5f55be0327d3223059afd7363a14
Marcos Rigel <marcosrigel1999@gmail.com>
2025-04-02 13:47:10 -0300

    mesclando o auxiliar com o projeto

--- projeto.py
+++ projeto.py
@@ -1,10 +1,24 @@
 import pandas as pd
+import pywhatkit
+import datetime
+import time
+import re
 
 df = pd.read_csv('https://docs.google.com/spreadsheets/d/1p5GkS4ngPcai_U8p2rOKnj3_kiefYYv383L6oevaNmM/export?format=csv')
 
-resultado = df[df['ID'] == 1]
-
-telefone = resultado['Telefone'].values[0]
+resultado = df[df['ID'] == 2]
 nome = resultado['Nome'].values[0]
-print(f"Telefone {nome}: {telefone}")
+telefone = resultado['Telefone'].values[0]
 
+numero = re.sub(r'\D', '', telefone)
+
+hora_atual = datetime.datetime.now()
+numero_da_hora = hora_atual.hour
+numero_do_minuto = hora_atual.minute
+
+minuto_mais_1 = numero_do_minuto + 1
+
+if minuto_mais_1 == 60:
+    minuto_mais_1 = 0
+
+pywhatkit.sendwhatmsg(f"+55{numero}", f"Ola {nome}, Esta é uma mensagem programada", numero_da_hora, minuto_mais_1)

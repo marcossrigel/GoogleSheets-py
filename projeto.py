785cefd04e665e0940036efb7e157ebf0614c1fc
Marcos Rigel <marcosrigel1999@gmail.com>
2025-04-02 16:35:49 -0300

    melhor versao 1.2

--- projeto.py
+++ projeto.py
@@ -1,9 +1,6 @@
-from datetime import datetime
 import pandas as pd
 import pyautogui
-import pyperclip
 import webbrowser
-import time
 import re
 
 url = 'https://docs.google.com/spreadsheets/d/1p5GkS4ngPcai_U8p2rOKnj3_kiefYYv383L6oevaNmM/export?format=csv'
@@ -13,7 +10,5 @@ resultado = df.loc[df['ID'] == 4].squeeze()
 nome = resultado['Nome']
 numero = re.sub(r'\D', '', resultado['Telefone'])
 
-link = f"https://web.whatsapp.com/send?phone={numero}&text=Olá {nome}, esta é uma mensagem automática :)"
-
-webbrowser.open(link)
+webbrowser.open(f"https://web.whatsapp.com/send?phone={numero}&text=Olá {nome}, esta é uma mensagem automática :)")
 pyautogui.press('enter')

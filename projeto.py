5ef4fb769c3dc071afb77d5be3ef52ccefe58bd8
Marcos Rigel <marcosrigel1999@gmail.com>
2025-04-03 13:18:11 -0300

    melhor versao 2.0

--- projeto.py
+++ projeto.py
@@ -3,16 +3,20 @@ import pyautogui
 import webbrowser
 import time
 import re
+from datetime import datetime
 
 url = 'https://docs.google.com/spreadsheets/d/1p5GkS4ngPcai_U8p2rOKnj3_kiefYYv383L6oevaNmM/export?format=csv'
-df = pd.read_csv(url, usecols=['ID', 'Nome', 'Telefone'])
 
-resultado = df.loc[df['ID'] == 3].squeeze()
+df = pd.read_csv(url, usecols=['Nome', 'Telefone', 'Data'])
+df['Data'] = pd.to_datetime(df['Data'], dayfirst=True, errors='coerce')
+hoje = pd.to_datetime(datetime.today().date())
 
-nome = resultado['Nome']
-numero = re.sub(r'\D', '', resultado['Telefone'])
+df_hoje = df[df['Data'] == hoje]
 
-webbrowser.open(f'https://web.whatsapp.com/send?phone={numero}&text=Olá {nome}, esta é uma mensagem automática :)')
-
-time.sleep(5)
-pyautogui.press('enter')
+for _, row in df_hoje.iterrows():
+    nome = row['Nome']
+    numero = re.sub(r'\D', '', row['Telefone'])
+    webbrowser.open(f'https://web.whatsapp.com/send?phone={numero}&text=Olá {nome}, esta é uma mensagem automática :)')
+    time.sleep(5)
+    pyautogui.press('enter')
+    time.sleep(3)
